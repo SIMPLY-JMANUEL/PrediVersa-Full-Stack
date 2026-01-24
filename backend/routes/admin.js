a15870da194434eaa8d1179ac0a3902b31f66a12
AndreLunSan <hals_93@hotmail.es>
2026-01-24 09:08:58 -0500

    Ajuste BD

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, createUser, getUserById } = require('../models/UserSQL');
+const { getAllUsers, createUser, getUserById } = require('../models/UserMySQL');
 
 // Helper para respuestas uniformes
 function sendResponse(
